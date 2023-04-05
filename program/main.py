f5fe937c13f252a2b1b138a53a5853a4358eaa32
ilyacapitoliy <60826461+ilyacapitoliy@users.noreply.github.com>
2023-04-06 02:28:14 +0400

    while loop

--- program/main.py
+++ program/main.py
@@ -34,53 +34,55 @@ if __name__ == "__main__":
             send_message(f"Error closing all positions {e}")
             exit(1)
     
-    # Find Cointegrated Pairs
-    if FIND_COINTEGRATED:
-
-        # Construct Market Prices
-        try:
-            print("Fetching market prices, please allow 3 min...")
-            df_market_prices = construct_market_prices(client)
-        except Exception as e:
-            print("Error constructing market prices: ", e)
-            send_message(f"Error constructing market prices {e}")
-            exit(1)
-        
-            # Store Cointegrated Pairs
-        try:
-            print("Storing cointegrated pairs...")
-            stores_result = store_cointegration_results(df_market_prices)
-            if stores_result != "saved":
-                print("Error saving cointegrated pairs")
-                exit(1)
-        except Exception as e:
-            print("Error saving cointegrated pairs: ", e)
-            send_message(f"Error saving cointegrated pairs {e}")
-            exit(1)
-    
-
-    # Run as always on
     while True:
 
-        # Place trades for opening positions
-        if MANAGE_EXITS:
+        # Find Cointegrated Pairs
+        if FIND_COINTEGRATED:
+
+            # Construct Market Prices
             try:
-                print("Managing exits...")
-                manage_trade_exits(client)
+                print("Fetching market prices, please allow 3 min...")
+                df_market_prices = construct_market_prices(client)
             except Exception as e:
-                print("Error managing exiting positions: ", e)
-                send_message(f"Error managing exiting positions {e}")
+                print("Error constructing market prices: ", e)
+                send_message(f"Error constructing market prices {e}")
+                exit(1)
+            
+                # Store Cointegrated Pairs
+            try:
+                print("Storing cointegrated pairs...")
+                stores_result = store_cointegration_results(df_market_prices)
+                if stores_result != "saved":
+                    print("Error saving cointegrated pairs")
+                    exit(1)
+            except Exception as e:
+                print("Error saving cointegrated pairs: ", e)
+                send_message(f"Error saving cointegrated pairs {e}")
                 exit(1)
         
+
+        # Run as always on
+        while True:
+
+            # Place trades for opening positions
+            if MANAGE_EXITS:
+                try:
+                    print("Managing exits...")
+                    manage_trade_exits(client)
+                except Exception as e:
+                    print("Error managing exiting positions: ", e)
+                    send_message(f"Error managing exiting positions {e}")
+                    exit(1)
+            
+            
+            
+            # Place trades for opening positions
+            if PLACE_TRADES:
+                try:
+                    print("Finding trading opportunities...")
+                    open_positions(client)
+                except Exception as e:
+                    print("Error trading pairs: ", e)
+                    send_message(f"Error opening trades {e}")
+                    exit(1)
         
-        
-        # Place trades for opening positions
-        if PLACE_TRADES:
-            try:
-                print("Finding trading opportunities...")
-                open_positions(client)
-            except Exception as e:
-                print("Error trading pairs: ", e)
-                send_message(f"Error opening trades {e}")
-                exit(1)
-    
